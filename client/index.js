0890b59b240cf058a07e6459431515ca75421655
Manik Roy <cm.dpi15@gmail.com>
2020-06-13 00:33:27 +0600

    setup project and apollo provider

--- client/index.js
+++ client/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
-const Root = () => {
-  return <div>Lyrical</div>
-};
+const client = new ApolloClient({});
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
+const Root = () => (
+  <ApolloProvider store={client}>
+    <div className="container center">
+      <h2>Lyrical Music</h2>
+    </div>
+  </ApolloProvider>
 );
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
+/* eslint-disable import/order */
 const express = require('express');
-const models = require('./models');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const models = require('./models');
 const schema = require('./schema/schema');
 
+dotenv.config({ path: './config.env' });
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = process.env.DATABASE;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
@@ -16,18 +19,23 @@ if (!MONGO_URI) {
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
+
 const webpackConfig = require('../webpack.config.js');
+
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
